4b3f14611b2fbf10b4f81c0e6db44973b74fd55e
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-04-27 19:28:45 +1000

    Also hook up stdin on Windows - this fixes interactive processes
    I could have sworn the docs said this would be done automatically,
    but it appears that’s not the case.

--- exec_windows.go
+++ exec_windows.go
@@ -14,6 +14,7 @@ func ExecCommand(args []string) {
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
 	err := cmd.Run()
 
 	if err != nil {
